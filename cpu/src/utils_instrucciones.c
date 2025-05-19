f725db2a0b2189ba1e8b4bf31651ff6d43bb4845
utnso <srojaspereira@frba.utn.edu.ar>
2025-05-19 00:36:25 +0000

    Primera implementación del ciclo de instrucciones en CPU

--- cpu/src/utils_instrucciones.c
+++ cpu/src/utils_instrucciones.c
@@ -25,6 +25,70 @@ bool recibirPIDyPC_kernel(int socket_kernel_dispatch, PCB_cpu *proc_AEjecutar) {
 
 void preparar_PCB_cpu(PCB_cpu *proc_AEjecutar) {}
 
+void ejecutarInstruccion(int socket_memoria, PCB_cpu *proc_AEjecutar, bool *fin_ejecucion) {
+    // CICLO DE INSTRUCCION //
+
+    // FETCH: busca la instrucción a memoria //
+    pedirInstruccionAMemoria(socket_memoria, proc_AEjecutar->pc);
+    t_list *instruccion = recibirInstruccionMemoria(socket_memoria); // Bloqueante: memoria le responde con un paquete en forma de lista (INSTRUCCION)
+
+    // DECODE: interpreta la instruccion recibida como 'string' y la convierte en 'enum' //
+    tipo_de_instruccion = interpretarInstruccion((char*)list_get(instruccion, 1));
+    
+    // EXECUTE: ejecuta la instrucción recibida //
+    switch(tipo_de_instruccion)
+    {
+        case INSTR_NOOP:
+        {
+            //ejecutar_instruccion_noop();
+            break;
+        }
+        case INSTR_WRITE:
+        {
+            //ejecutar_instruccion_write();
+            break;
+        }
+        case INSTR_READ:
+        {
+            //ejecutar_instruccion_read();
+            break;
+        }
+        case INSTR_GOTO:
+        {
+            //ejecutar_instruccion_goto();
+            break;
+        }
+        case INSTR_IO:
+        {
+            //ejecutar_instruccion_io();
+            break;
+        }
+        case INSTR_INIT_PROC:
+        {
+            //ejecutarInstruccion_init_proc();
+            break;
+        }
+        case INSTR_DUMP_MEMORY:
+        {
+            //ejecutarInstruccion_dump_memory();
+            break;
+        }
+        case INSTR_EXIT:
+        {
+            //ejecutarInstruccion_exit();
+            break;
+        }
+        default:
+        {
+            //
+            break;
+        }
+    }
+    
+    //TODO: liberar instruccion_recibida
+    
+    controlarInterrupciones();
+}
 
 
 void pedirInstruccionAMemoria(int socket_memoria, int programCounter) {
