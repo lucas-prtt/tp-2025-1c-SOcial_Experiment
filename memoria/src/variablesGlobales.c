870a53d4ee90cfb1bd9f56b495e6996aab1c15f6
lucas-prtt <lpresotto@frba-utn-edu.ar>
2025-06-08 22:15:25 +0000

    Globalizacion de variables y wrappers para operar con PID

--- memoria/src/variablesGlobales.c
+++ memoria/src/variablesGlobales.c
@@ -1,5 +1,12 @@
 #include "variablesGlobales.h"
 
+
+t_list * tablaDeProcesos = NULL;
+
+
+
+//////////////////// ARBOL INICIO ////////////////////
+
 void ** crearNivelTablaDePaginas(int maximoEntradasTabla){
     void ** tabla = malloc(sizeof(void*)*maximoEntradasTabla);
     for(int i=0; i<maximoEntradasTabla; i++)
@@ -31,17 +38,17 @@ int * buscarOCrear(void ** arbolDePaginas, t_list * entradas, int niveles, int m
 
 }
 
-void asignarMarcoAPagina(int numeroMarco, void ** arbolDePaginas, int maximoEntradasTabla, t_list * entradas, int niveles)
+void asignarMarcoAPagina(int numeroMarco, void ** arbolDePaginas, t_list * entradas)
 {
-    int * ubicacion = buscarOCrear (arbolDePaginas, entradas, niveles, maximoEntradasTabla);
+    int * ubicacion = buscarOCrear (arbolDePaginas, entradas, nivelesTablas, maximoEntradasTabla);
     *ubicacion = numeroMarco;
 }
-int leerMarcoDePagina(int numeroMarco, void ** arbolDePaginas, int maximoEntradasTabla, t_list * entradas, int niveles)
+int leerMarcoDePagina(void ** arbolDePaginas, t_list * entradas)
 {
-    int * ubicacion = buscarOCrear (arbolDePaginas, entradas, niveles, maximoEntradasTabla);
+    int * ubicacion = buscarOCrear (arbolDePaginas, entradas, nivelesTablas, maximoEntradasTabla);
     return * ubicacion;
 }
-void liberarArbolDePaginas(void ** arbolDePaginas, int maximoEntradasTabla, int nivelesRestantes){
+void liberarArbolDePaginasAux(void ** arbolDePaginas, int nivelesRestantes){
     if (arbolDePaginas == NULL){
         return;
     }
@@ -53,8 +60,64 @@ void liberarArbolDePaginas(void ** arbolDePaginas, int maximoEntradasTabla, int 
         }
     }else{
         for(int i=0; i<maximoEntradasTabla; i++)
-            liberarArbolDePaginas((void**)(arbolDePaginas[i]), maximoEntradasTabla, nivelesRestantes-1);
+            liberarArbolDePaginasAux((void**)(arbolDePaginas[i]), nivelesRestantes-1);
     }   
     free(arbolDePaginas);
     return;
 }
+
+void liberarArbolDePaginas(void ** arbolDePaginas){
+    liberarArbolDePaginasAux(arbolDePaginas, nivelesTablas);
+}
+
+//////////////////// ARBOL FIN //////////////////
+
+
+/////////////////// TP INCIO /////////////////
+
+void agregarProcesoATabla(int nuevoPID){
+    PIDyTP * nuevoElemento = malloc(sizeof(PIDyTP));
+    nuevoElemento->PID = nuevoPID;
+    nuevoElemento->TP = crearNivelTablaDePaginas(maximoEntradasTabla);
+    list_add(tablaDeProcesos, nuevoElemento);
+    
+}
+PIDyTP * obtenerProcesoYTPConPID(int PIDBuscado){
+    #ifndef __INTELLISENSE__
+    bool coincide (void * elemento)
+    {
+        return ((PIDyTP*)elemento)->PID == PIDBuscado;
+    }
+    return (PIDyTP*)list_find(tablaDeProcesos, coincide);
+    #endif
+}
+PIDyTP * removerProcesoYTPConPID(int PIDBuscado){
+    #ifndef __INTELLISENSE__
+    bool coincide (void * elemento)
+    {
+        return ((PIDyTP*)elemento)->PID == PIDBuscado;
+    }
+    return (PIDyTP*)list_remove_by_condition(tablaDeProcesos, coincide);
+    #endif
+}
+
+void eliminarProcesoDeTabla(int PIDEliminado){
+    PIDyTP * elemento = removerProcesoYTPConPID(PIDEliminado);
+    liberarArbolDePaginas(elemento->TP);
+    free(elemento);
+}
+int obtenerMarcoDePaginaConPIDYEntradas(int PID, t_list * entradas){
+    return leerMarcoDePagina(obtenerProcesoYTPConPID(PID)->TP, entradas);
+}
+void asignarMarcoAPaginaConPIDyEntradas(int PID, t_list * entradas, int marco){
+    asignarMarcoAPagina(marco, obtenerProcesoYTPConPID(PID)->TP, entradas);
+    return; 
+}
+void inicializarVariablesGlobales(int sizeTabla, int qNiveles){
+    tablaDeProcesos = list_create();
+    maximoEntradasTabla = sizeTabla;
+    nivelesTablas = qNiveles;
+    // TODO: Inicializar void* memoriaDeUsuario segun el tamaño
+}
+
+/////////////////// TP FIN /////////////////////
--- memoria/src/variablesGlobales.h
+++ memoria/src/variablesGlobales.h
@@ -3,6 +3,8 @@
 #include "commons/collections/list.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
 typedef struct PIDyTablaDePaginas{
     int PID;
     void** TP;
@@ -11,5 +13,19 @@ typedef struct PIDyTablaDePaginas{
 
 extern void* memoriaDeUsuario;
 extern t_list* tablaDeProcesos; // Llena de PIDyTablaDePaginas
+extern int maximoEntradasTabla;
+extern int nivelesTablas;
+extern int tamañoMarcos;
+extern int tamañoMemoriaDeUsuario;
+
+void inicializarVariablesGlobales(int sizeTabla, int qNiveles);
+int obtenerMarcoDePaginaConPIDYEntradas(int PID, t_list * entradas);
+void asignarMarcoAPaginaConPIDyEntradas(int PID, t_list * entradas, int marco);
+void eliminarProcesoDeTabla(int PIDEliminado);
+void agregarProcesoATabla(int nuevoPID);
+
+
+
+
 
 #endif
