982cf1345e1a21aaf86bdca84d60f5740f622fa7
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-07-09 15:35:31 +0000

    mas prolijo

--- execAll.sh
+++ execAll.sh
@@ -6,18 +6,21 @@
 
 # Módulo: memoria
 cd memoria
+rm memory.log
 make
 gnome-terminal --title="Memoria" -- bash -c "./bin/memoria"
 cd ..
 
 # Módulo: kernel
 cd kernel
+rm kernel.log
 make
 gnome-terminal --title="Kernel" -- bash -c "./bin/kernel PLANI_CORTO_PLAZO 0"
 cd ..
 
 # Módulo: cpu
 cd cpu
+rm cpu_1.log
 make
 gnome-terminal --title="Cpu" -- bash -c "./bin/cpu 1"
 cd ..
@@ -25,6 +28,7 @@ cd ..
 
 # Módulo: io
 cd io
+rm io.log
 make
 gnome-terminal --title="IO" -- bash -c "./bin/io DISCO"
 cd ..
