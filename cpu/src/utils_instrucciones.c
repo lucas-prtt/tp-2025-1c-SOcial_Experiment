6cbb138c218fb6e85c37ffa248112b11ec3a7d55
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-07-09 15:47:02 +0000

    Identificacion del error

--- cpu/src/utils_instrucciones.c
+++ cpu/src/utils_instrucciones.c
@@ -339,7 +339,7 @@ bool checkInterrupt(cpu_t *cpu) {
 }
 
 void devolverProcesoKernel(int socket_kernel, PCB_cpu *proc_AEjecutar) {
-    t_paquete *paquete_devolucion_proceso = crear_paquete(INTERRUPT_ACKNOWLEDGE);
+    t_paquete *paquete_devolucion_proceso = crear_paquete(INTERRUPT_ACKNOWLEDGE); // ACA ESTA EL ERRORRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR
     log_trace(logger, "El mensaje dice: <El proceso que se estaba ejecutando quedo en PC: %d>", proc_AEjecutar->pc);
     agregar_a_paquete(paquete_devolucion_proceso, &(proc_AEjecutar->pc), sizeof(proc_AEjecutar->pc));
     enviar_paquete(paquete_devolucion_proceso, socket_kernel);
