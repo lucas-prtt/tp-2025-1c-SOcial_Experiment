e74793dac62b4da52e9c9463f1396403c63fe94d
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-07-13 17:17:33 +0000

    Version pruebas con terminal preinstalada

--- execAll.sh
+++ execAll.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# Requiere instalarse gnome terminal
 # Ejecuta todo en la misma compu
 # Sirve para pruebas rapidas, pero es mejor usar un entorno distribuido
 # Ejecutable con ./execAll.sh
@@ -11,22 +10,21 @@ echo -n "x" >> ejecuciones.log
 cd memoria
 rm memory.log
 make
-gnome-terminal --title="Memoria" -- bash -c "./bin/memoria"
+exo-open --launch TerminalEmulator --title="Memoria" -e "bash -c './bin/memoria;'"
 cd ..
 
 # Módulo: kernel
 cd kernel
 rm kernel.log
 make
-gnome-terminal --title="Kernel" -- bash -c "./bin/kernel PLANI_CORTO_PLAZO 0"
+exo-open --launch TerminalEmulator --title="Kernel" -e "bash -c './bin/kernel PLANI_CORTO_PLAZO 0'"
 cd ..
 
 # Módulo: cpu
 cd cpu
 rm cpu_1.log
 make
-gnome-terminal --title="Cpu" -- bash -c "./bin/cpu 1"
-#gnome-terminal --title="Cpu" -- bash -c "./bin/cpu 2"
+exo-open --launch TerminalEmulator --title="Cpu" -e "bash -c './bin/cpu 1'"
 cd ..
 
 
@@ -34,6 +32,5 @@ cd ..
 cd io
 rm io.log
 make
-gnome-terminal --title="IO" -- bash -c "./bin/io DISCO"
-#gnome-terminal --title="IO" -- bash -c "./bin/io DISCO"
+exo-open --launch TerminalEmulator --title="IO" -e "bash -c './bin/io DISCO'"
 cd ..
