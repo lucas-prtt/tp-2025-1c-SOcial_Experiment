e1b707d8dddb1d368d1af4c62d84311c08d8311a
lucas-prtt <lpresotto@frba-utn-edu.ar>
2025-07-16 21:09:43 +0000

    Version que deberia andarle a todos

--- execAll.sh
+++ execAll.sh
@@ -1,38 +1,34 @@
 #!/bin/bash
-# Ejecuta todo en la misma compu
-# Sirve para pruebas rapidas, pero es mejor usar un entorno distribuido
-# Ejecutable con ./execAll.sh
 
-# Registra cuantas pruebas hiciste, para que tu decenso a la locura quede justificado
+# Log the run
 echo -n "x" >> ejecuciones.log
 
 # Módulo: memoria
 cd memoria
-rm memory.log
+rm -f memory.log
 make
-exo-open --launch TerminalEmulator --title="Memoria" -e "bash -c 'valgrind ./bin/memoria; read -n 1'"
+xterm -T "Memoria" -e bash -c "valgrind ./bin/memoria; read -n 1" &
 cd ..
 sleep 1
 
 # Módulo: kernel
 cd kernel
-rm kernel.log
+rm -f kernel.log
 make
-exo-open --launch TerminalEmulator --title="Kernel" -e "bash -c 'valgrind ./bin/kernel MEMORIA_IO 90; read -n 1';"
+xterm -T "Kernel" -e bash -c "valgrind ./bin/kernel MEMORIA_IO 90; read -n 1" &
 cd ..
 sleep 1
 
 # Módulo: cpu
 cd cpu
-rm cpu1.log
+rm -f cpu1.log
 make
-exo-open --launch TerminalEmulator --title="Cpu" -e "bash -c 'valgrind ./bin/cpu 1; read -n 1;'"
+xterm -T "Cpu" -e bash -c "valgrind ./bin/cpu 1; read -n 1" &
 cd ..
-
+sleep 1
 
 # Módulo: io
 cd io
-rm io.log
+rm -f io.log
 make
-exo-open --launch TerminalEmulator --title="IO" -e "bash -c 'valgrind ./bin/io DISCO; read -n 1;'"
-cd ..
+xterm -T "IO" -e bash -c "valgrind ./bin/io DISCO"
