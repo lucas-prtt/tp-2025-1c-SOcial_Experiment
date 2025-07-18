6dc78f563dac720d3b5c9dfd32868475a0c21fc2
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-07-18 23:49:05 +0000

    Fix invelid read and free en eliminarListaYElementos

--- cpu/src/utils_instrucciones.c
+++ cpu/src/utils_instrucciones.c
@@ -43,8 +43,6 @@ bool ejecutarCicloInstruccion(cpu_t *cpu, PCB_cpu *proc_AEjecutar) {
     
     if(fin_proceso) {
         log_trace(logger, "Interrumpo ejecucion, le mando un mensaje a kernel");
-        free(instruccion);
-        list_destroy_and_destroy_elements(instruccion_list, free);
         log_trace(logger, "Aquí me voy!");
         if(interrupcion && ultimaInstruccionInitProc) { // Si a kernel le hice syscall de INIT_PROC, automaticamente me manda el mismo proceso.
             t_list * ConfirmacionSyscallRecibido = recibir_paquete_lista(cpu->socket_kernel_dispatch, MSG_WAITALL, NULL);
