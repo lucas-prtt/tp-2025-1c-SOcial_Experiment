e5cf9e6f0ecce79464db489a53f19d41f35cd42e
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-07-31 15:15:56 +0000

    Fix booleano que no se usaba para mostrar la consola

--- utils/src/utils/logConfig.c
+++ utils/src/utils/logConfig.c
@@ -4,7 +4,7 @@ t_config * config;
 int abrirConfigYLog(char* configName, char* logName, char * processName, bool console){
     config = config_create(configName);
     if (config == NULL) { return 1; }
-    logger = log_create(logName, processName, false, log_level_from_string(config_get_string_value(config, "LOG_LEVEL")));
+    logger = log_create(logName, processName, console, log_level_from_string(config_get_string_value(config, "LOG_LEVEL")));
     if (logger == NULL) { config_destroy(config); return 2; }
     log_debug(logger, "================ Log Iniciado ================");
     return 0;
