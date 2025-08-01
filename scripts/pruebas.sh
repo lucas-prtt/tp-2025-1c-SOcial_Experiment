8005f8ed434c93fe936d49e705a30d376a0a04b8
lucas-prtt <lpresotto@frba.utn.edu.ar>
2025-08-01 16:29:31 +0000

    Cambio script descargar pruebas para que use https en lugar de ssh

--- scripts/pruebas.sh
+++ scripts/pruebas.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd ..
 echo "Acordate que la password en realidad es el token"
-git clone git@github.com:sisoputnfrba/revenge-of-the-cth-pruebas.git
+git clone https://github.com/sisoputnfrba/revenge-of-the-cth-pruebas.git
 
